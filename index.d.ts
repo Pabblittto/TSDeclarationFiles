4352e34dadfe06b9155a608d3c7b6896f6923e27
Pawel Laskarzewski <pawel.mat17@op.pl>
2021-12-16 23:59:36 +0100

    types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 declare module "*.png";
 declare module "@pabblo/libraryforpresentation" {
-  export declare const addNumbers: (a: number, b: number) => number;
+  export declare const addNumbers: (a: number) => number;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { ConstantInterface } from "@pabblo/libraryforpresentation-ts";
+
 import { addNumbers } from "@pabblo/libraryforpresentation";
 
-declare module "@pabblo/libraryforpresentation-ts" {
-  interface ConstantInterface {
-    additional: number;
-  }
+interface extended extends ConstantInterface {
+  asdF: number;
 }
+
+const example: extended;
+
+// declare module "@pabblo/libraryforpresentation-ts" {
+//   interface ConstantInterface {
+//     additional: number;
+//   }
+// }
